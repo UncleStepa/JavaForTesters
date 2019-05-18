770ba89671da1d8f5a5d33e45b51058abc269a78
UncleStepa <stankevichii@gmail.com>
2019-05-18 23:07:26 +0400

    Решение 2-го задания без метода в класе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,8 +1,12 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  double x1;
-  double y1;
-  double x2;
-  double y2;
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/StartPoint.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/StartPoint.java
@@ -4,17 +4,16 @@ import static java.lang.Math.sqrt;
 
 public class StartPoint {
   public static void main(String[] args) {
-    Point p1 = new Point();
-    p1.x1 = 2.0;
-    p1.y1 = 3.0;
-    Point p2 = new Point();
-    p2.x2 = 4.0;
-    p2.y2 = 7.0;
-    System.out.println(distance(p1, p2));
 
+    Point p1 = new Point(2.0, 3.0);
+    Point p2 = new Point(4.0, 7.0);
+
+    System.out.println(distance(p1, p2));
   }
+
   public static double distance(Point p1, Point p2) {
 
-    return sqrt((p2.x2 - p1.x1) * (p2.x2 - p1.x1) + (p2.y2 - p1.y1) * (p2.y2 - p1.y1));
+    return sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
   }
+
 }
