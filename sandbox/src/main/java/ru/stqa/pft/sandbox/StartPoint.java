35462ca0097e0eb285c54efb828b09d415b63d50
UncleStepa <stankevichii@gmail.com>
2019-05-18 23:16:36 +0400

    Решение 2-го задания с добавлением метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/StartPoint.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/StartPoint.java
@@ -1,19 +1,12 @@
 package ru.stqa.pft.sandbox;
 
-import static java.lang.Math.sqrt;
+
 
 public class StartPoint {
   public static void main(String[] args) {
 
-    Point p1 = new Point(2.0, 3.0);
-    Point p2 = new Point(4.0, 7.0);
+    Point coordinates = new Point(2.0, 3.0, 4.0, 6.0);
+    System.out.println(coordinates.distance());
 
-    System.out.println(distance(p1, p2));
   }
-
-  public static double distance(Point p1, Point p2) {
-
-    return sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-  }
-
 }
