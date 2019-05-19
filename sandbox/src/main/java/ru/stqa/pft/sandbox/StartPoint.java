dc5d44f2798b27ef22ca84f5a9aaa77b4ff281a4
UncleStepa <stankevichii@gmail.com>
2019-05-19 13:41:46 +0400

    Исправил 2-е задание с учетом замечания.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/StartPoint.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/StartPoint.java
@@ -5,8 +5,10 @@ package ru.stqa.pft.sandbox;
 public class StartPoint {
   public static void main(String[] args) {
 
-    Point coordinates = new Point(2.0, 3.0, 4.0, 6.0);
-    System.out.println(coordinates.distance());
+    Point p1 = new Point(3.0, 4.0);
+    Point p2 = new Point(5.0, 6.0);
+
+    System.out.println(p1.distance(p2));
 
   }
 }
