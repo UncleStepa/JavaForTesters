e846c4168496c07d68ce7ddf5489d96be83c9dfe
UncleStepa <stankevichii@gmail.com>
2019-07-03 08:40:20 +0400

    Добавил findElments для тестов с группами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteOneGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteOneGroup.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,12 +9,15 @@ public class DeleteOneGroup extends TestBase {
   @Test
   public void testDeleteOneGroup() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    if (! app.getGroupHelper().isThereaGroup()) {
+    int before = app.getGroupHelper().getGroupCount();
+    if (!app.getGroupHelper().isThereaGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test1"));
     }
     app.getGroupHelper().selectgroup();
     app.getGroupHelper().deleteSelectedGroup();
     app.getGroupHelper().returnToGroupPage();
-  }
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before - 1);
 
+  }
 }
