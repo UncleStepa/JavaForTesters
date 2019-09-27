329d3285cbf3127ffb585a8c2d5ba8949974102e
UncleStepa <stankevichii@gmail.com>
2019-09-27 13:47:58 +0400

    5.10 Режем строки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -1,11 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContractInfo;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -31,8 +29,12 @@ public class ContactPhoneTests extends TestBase{
   public void testContactPhones() {
     ContractInfo contact = app.contact().all().iterator().next();
     ContractInfo contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
-    assertThat(contact.getHome_phone(), equalTo(contactInfoFromEditForm.getHome_phone()));
-    assertThat(contact.getMobile_phone(), equalTo(contactInfoFromEditForm.getMobile_phone()));
-    assertThat(contact.getWork_phone(), equalTo(contactInfoFromEditForm.getWork_phone()));
+    assertThat(contact.getHome_phone(), equalTo(cleaned(contactInfoFromEditForm.getHome_phone())));
+    assertThat(contact.getMobile_phone(), equalTo(cleaned(contactInfoFromEditForm.getMobile_phone())));
+    assertThat(contact.getWork_phone(), equalTo(cleaned(contactInfoFromEditForm.getWork_phone())));
+  }
+
+  public String cleaned(String phone) {
+    return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
   }
 }
